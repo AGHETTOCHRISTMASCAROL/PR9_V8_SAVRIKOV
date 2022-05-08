3127f6ade6a28e20dba7e4f812a7d27e74fd1710
A GHETTO CHRISTMAS CAROL <ivansavrikow@gmail.com>
2022-05-08 17:00:42 +0700

    готово

---
+++ app/src/main/java/com/example/pr9_v8/Screan_3.kt
@@ -0,0 +1,11 @@
+package com.example.pr9_v8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Screan_3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screan3)
+    }
+}
